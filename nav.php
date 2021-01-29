19ab1214e27a034560543bfac10dfbe1efae7732
furrerservices <40439762+furrerservices@users.noreply.github.com>
2021-01-29 20:14:02 +0100

    upload test
    initial upload for test

---
+++ nav.php
@@ -0,0 +1,5 @@
+<div>
+	<a class="button" href="index.php">Home</a>
+	<a class="button" href="registrieren.php">Registrieren</a>
+	<a class="button" href="login.php">Login</a>
+</div>
--- registrieren.php
+++ registrieren.php
@@ -8,11 +8,11 @@ die('<h1>Für diese Anwendung ' .
 'ist mindestens PHP 5 notwendig</h1>');
 }
 ?>
-﻿<!DOCTYPE HTML>
+<!DOCTYPE HTML>
 <html lang="de">
 <head>
 	<meta charset="utf-8" />
- 	<title>Image2Food - Home</title>
+ 	<title>Image2Food - Registrierung</title>
 	<meta name="viewport" content="width=device-width; initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="styles.css" />
 </head>
@@ -20,15 +20,15 @@ die('<h1>Für diese Anwendung ' .
 	<div id="nav"> <?php require_once("nav.php");?> </div>
 	<div id="content">
 		<h1>Image2Food - sag mir was ich daraus kochen kann</h1>
-		<h2>Das soiale, multimediale Netzwerk für Kochideen</h2>
+		<h2>Registrierung</h2>
 		<?php
 		/***
 		* Das soziale Netzwerk für Kochideen
-		* Die Einstiegsseite mit der Hauptklasse
+		* Die Registrierungsseite
 		*/
-		class Index{
+		class Registrierung{
 		}
 		?>
-	</div>
+</div>
 </body>
 </html>
